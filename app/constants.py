ca88626e087e467553c526e8487fc8b6531780ed
sohombhattacharya <sohom95@yahoo.com>
2018-01-15 16:38:21 -0500

    built working beer search endpoint /search/beers

---
+++ app/constants.py
@@ -0,0 +1,2 @@
+API_URL = 'http://api.brewerydb.com/v2'
+API_KEY = 'c510284857bf65cf054e40dea79de1a6'
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
 from app import app
-from flask import jsonify
+from flask import jsonify, request
+from .constants import *
+import requests
+
 @app.route('/')
 @app.route('/index')
 def index():
     return jsonify({'text': "Hello World"})
+
+@app.route('/search/beers', methods = {'POST'})
+def searchBeers():
+    print (request.get_json())
+    searchStr = '%s/search?q=%s&type=beer&key=%s' % (API_URL, request.get_json()['q'], API_KEY)
+    print (searchStr)
+    r = requests.get(searchStr)
+    if r.status_code == 200:
+        return jsonify({"status": "success", "result": r.json()})
+    else:
+        return jsonify({"status": "failure", "error code": r.status_code})
+
